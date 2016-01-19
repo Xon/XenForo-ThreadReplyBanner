439b89c202b6ae4eb5e984d6c843970a7e10ea2f
Xon <tazipper1@hotmail.com>
2016-01-19 08:06:10 +0800

    Correct typo preventing a thread being hard-deleted (or a thread redirect being deleted)

--- upload/library/SV/ThreadReplyBanner/XenForo/DataWriter/Discussion/Thread.php
+++ upload/library/SV/ThreadReplyBanner/XenForo/DataWriter/Discussion/Thread.php
@@ -8,50 +8,51 @@ class SV_ThreadReplyBanner_XenForo_DataWriter_Discussion_Thread extends XFCP_SV_
 
     protected function _discussionPreSave()
     {
-        $ret = parent::_discussionPreSave();
-        if (!empty(SV_ThreadReplyBanner_Globals::$controller))
+        parent::_discussionPreSave();
+        if (empty(SV_ThreadReplyBanner_Globals::$controller))
         {
-            $threadModel = $this->_getThreadModel();
-            $thread = $this->getMergedData();
-            $forum = $this->_getForumData();
+            return;
+        }
+        $threadModel = $this->_getThreadModel();
+        $thread = $this->getMergedData();
+        $forum = $this->_getForumData();
 
-            if (empty($forum) || !$threadModel->canManageThreadReplyBanner($thread, $forum))
-            {
-                return $ret;
-            }
+        if (empty($forum) || !$threadModel->canManageThreadReplyBanner($thread, $forum))
+        {
+            return;
+        }
 
-            if ($this->isUpdate())
+        if ($this->isUpdate())
+        {
+            $old_banner = $threadModel->getRawThreadReplyBanner($this->get('thread_id'));
+        }
+        else
+        {
+            $old_banner = '';
+        }
+
+        $new_banner = SV_ThreadReplyBanner_Globals::$controller->getInput()->filterSingle('thread_reply_banner', XenForo_Input::STRING);
+        if($new_banner == $old_banner)
+        {
+            return;
+        }
+        if (strlen($new_banner) <= self::banner_length)
+        {
+            $this->new_banner = $new_banner;
+
+            if (empty($new_banner))
             {
-                $old_banner = $threadModel->getRawThreadReplyBanner($this->get('thread_id'));
+                XenForo_Model_Log::logModeratorAction('thread', $thread, 'replybanner_deleted');
             }
             else
             {
-                $old_banner = '';
-            }
-
-            $new_banner = SV_ThreadReplyBanner_Globals::$controller->getInput()->filterSingle('thread_reply_banner', XenForo_Input::STRING);
-            if($new_banner != $old_banner)
-            {
-                if (strlen($new_banner) <= self::banner_length)
-                {
-                    $this->new_banner = $new_banner;
-
-                    if (empty($new_banner))
-                    {
-                        XenForo_Model_Log::logModeratorAction('thread', $thread, 'replybanner_deleted');
-                    }
-                    else
-                    {
-                        XenForo_Model_Log::logModeratorAction('thread', $thread, 'replybanner', array('banner' => $new_banner));
-                    }
-                }
-                else
-                {
-                    $this->error(new XenForo_Phrase('please_enter_value_using_x_characters_or_fewer', array('count' => self::banner_length)));
-                }
+                XenForo_Model_Log::logModeratorAction('thread', $thread, 'replybanner', array('banner' => $new_banner));
             }
         }
-        return $ret;
+        else
+        {
+            $this->error(new XenForo_Phrase('please_enter_value_using_x_characters_or_fewer', array('count' => self::banner_length)));
+        }
     }
 
     protected function _discussionPostSave()
@@ -65,7 +66,7 @@ class SV_ThreadReplyBanner_XenForo_DataWriter_Discussion_Thread extends XFCP_SV_
 
     protected function _discussionPostDelete()
     {
-        parent::discussionPostDelete();
+        parent::_discussionPostDelete();
         $this->_db->query(
             'delete from xf_thread_banner where thread_id = ?'
         , $this->get('thread_id'));
